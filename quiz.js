7390755cf210b9fbd513416f67a130b12b7b62f3
donna <donnaxnguyen@yahoo.com>
2020-06-23 16:21:53 -0700

    committing files

---
+++ quiz.js
@@ -0,0 +1,23 @@
+/*
+Q1. How many whiskers does an average cat have?
+
+When does a cat purr?
+
+What is the average number a female cat can give birth to?
+
+What part of a cats body can appear and disappear?
+
+What was the punishment for killing a cat in Ancient Egypt?
+
+What body part do cats sweat out of?
+
+What smell is so gross to cat that they stay away from it?
+
+What flavor are cats unable to taste?
+*/
+
+/* this is the start of my questions */
+/* the title is what's being asked */
+/* choices are the multiple choices */
+/* answer is the answer */
+
