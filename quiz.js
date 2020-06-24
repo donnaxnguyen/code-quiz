f7e350c35186e32f97438cc5564815e1603bdac9
donna <donnaxnguyen@yahoo.com>
2020-06-23 17:26:14 -0700

    created questionnaire

--- quiz.js
+++ quiz.js
@@ -1,23 +1,47 @@
-/*
-Q1. How many whiskers does an average cat have?
-
-When does a cat purr?
-
-What is the average number a female cat can give birth to?
-
-What part of a cats body can appear and disappear?
-
-What was the punishment for killing a cat in Ancient Egypt?
-
-What body part do cats sweat out of?
-
-What smell is so gross to cat that they stay away from it?
-
-What flavor are cats unable to taste?
-*/
-
 /* this is the start of my questions */
 /* the title is what's being asked */
 /* choices are the multiple choices */
 /* answer is the answer */
 
+var questions = [{
+    title: "Q1. How many whiskers does an average cat have?",
+    choices: ["20", "15", "12", "8"],
+    answer: "12"
+    }
+    ,
+    {
+    title: "Q2. When does a cat purr?",
+    choices: ["When they're being petted'", "When they're content/happy", "When they're in pain and need to self-soothe", "All of the above"],
+    answer: "All of the above"
+    }
+    ,
+    {
+    title: "Q3. On average, how many cats can a cat mother give birth to?",
+    choices: ["1-2", "unlimited", "6-7", "3-5"],
+    answer: "3-5"
+    }
+    ,
+    {
+    title: "Q4. What part of a cats body can appear and disappear?",
+    choices: ["Their paws", "Their ears", "Their whiskers", "Their limbs"],
+    answer: "Their paws"
+    }
+    ,
+    {
+    title: "Q5. What color was the fictional cat Garfield in the 1980's comic?",
+    choices: ["White", "Grey", "Black", "Orange"],
+    answer: "Orange"
+    }
+    ,
+    {
+    title: "Q6. What flavor are cats unable to taste?",
+    choices: ["Sour", "Sweet", "Salty", "Bitter"],
+    answer: "Sweet"
+    }
+    ,
+    {
+    title: "Q7. What smell is so gross to cat that they stay away from it?",
+    choices: ["Catnip", "Floral Aromas", "Mint, Basil, Peppermint", "Citrus"],
+    answer: "Citrus"
+    }
+]
